62e57d37047d513c7ca9f0eba15712bd2001ac78
Aleksandra <77754618083@mail.ru>
2024-08-28 14:26:02 +0300

    created header, cardList and card

---
+++ src/components/CardList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert(111)
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="12999"
+      :onClickAdd="onClickAdd"
+    />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
